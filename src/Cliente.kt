171aec33cf2a0e37603569526e7dcf6de9fcbf85
dbenfon341 <dbenfon341@g.educaand.es>
2024-01-29 18:15:40 +0100

    Initial commit.

---
+++ src/Cliente.kt
@@ -0,0 +1,10 @@
+/**
+ * Clase Cliente
+ * @param nombre nombre del cliente
+ * @param domicilio domicilio del cliente
+ * @constructor Crea un cliente con nombre y domicilio
+ */
+class Cliente {
+    var nombre: String = ""
+    var domicilio: String = ""
+}
---
+++ src/Compra.kt
@@ -0,0 +1,12 @@
+/**
+ * Clase Compra
+ * @param cliente cliente que realizo la compra
+ * @param dia dia de la compra
+ * @param monto monto de la compra
+ * @constructor Crea una compra con cliente, dia y monto
+ */
+class Compra {
+    var cliente: String = ""
+    var dia: Int = 0
+    var monto: Double = 0.0
+}
